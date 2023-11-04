7748aa73580466beaa82c8ff2167b181f98856fc
khammad5 <khammadakh@code.edu.az>
2023-11-04 20:26:39 +0400

    Add project files.

---
+++ 12.05.2023/Program.cs
@@ -0,0 +1,21 @@
+﻿//#region 1ci
+////1) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+//int num1 = 7;
+//int num2 = 39;
+//int counter = 0;
+//string GettingNumbersBetweenOddNumbers(int x, int y, int counter)
+//{
+//    for (int i = x; i < y; i++)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            counter += i;
+//        }
+
+//    }
+//    return ($"{counter} bunlarin cemidir");
+//}
+
+//Console.WriteLine(GettingNumbersBetweenOddNumbers(num1, num2, counter));
+//#endregion
+
