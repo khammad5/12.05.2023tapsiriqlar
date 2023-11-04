fdc795476d29bd5bba27a0e543f611a1f4862c5b
khammad5 <khammadakh@code.edu.az>
2023-11-04 20:37:25 +0400

    2ci tapsiriq

--- 12.05.2023/Program.cs
+++ 12.05.2023/Program.cs
@@ -1,4 +1,4 @@
-﻿//#region 1ci
+﻿#region 1ci
 ////1) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
 //int num1 = 7;
 //int num2 = 39;
@@ -17,5 +17,28 @@
 //}
 
 //Console.WriteLine(GettingNumbersBetweenOddNumbers(num1, num2, counter));
-//#endregion
+#endregion
+
+#region 2ci
+////2)Verilmish arrayin icindeki tek ededlerin cemini tapin.
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 45, 67 };
+//int counter2 = 0;
+
+//string GettingNumbersBetweenOddNumbersFromArray(int[] numbers, int counter)
+//{
+//    foreach (int n in numbers)
+//    {
+//        if (n % 2 == 1)
+//        {
+//            counter += n;
+//        }
+//    }
+//    return ($"{counter}bunlarin cemini ifade dir");
+//}
+//Console.WriteLine(GettingNumbersBetweenOddNumbersFromArray(numbers, counter2));
+#endregion
+
+
+
+
 
